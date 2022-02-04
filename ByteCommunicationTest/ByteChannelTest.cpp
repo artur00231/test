a229d6e1fb2f26f0a70f6ea2479a82fead9a97a1
artur00231 <29777966+artur00231@users.noreply.github.com>
2022-02-04 02:15:34 +0100

    Update ByteChannelTest.cpp

--- ByteCommunicationTest/ByteChannelTest.cpp
+++ ByteCommunicationTest/ByteChannelTest.cpp
@@ -30,7 +30,7 @@ TEST(ByteChannel, BasicCommunication2)
 	reader.putByte('\12', ByteChannel::IOPoint::P2);
 	reader.putByte('\13', ByteChannel::IOPoint::P2);
 
-	EXPECT_FALSE(reader.isAvaiable(ByteChannel::IOPoint::P1));
+	EXPECT_TRUE(reader.isAvaiable(ByteChannel::IOPoint::P1));
 	EXPECT_FALSE(reader.isAvaiable(ByteChannel::IOPoint::P2));
 	EXPECT_EQ('\10', reader.getByte(ByteChannel::IOPoint::P1));
 	EXPECT_EQ('\11', reader.getByte(ByteChannel::IOPoint::P1));
